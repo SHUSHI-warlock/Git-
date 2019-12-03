bdfa0fcd8ea47f2a813084555805d0ba82e5539f
SHUSHI-warlock <43929384+SHUSHI-warlock@users.noreply.github.com>
2019-12-03 09:56:22 +0800

    加了头文件和cpp
    根据头文件写cpp！！！

---
+++ 计算机网络/计算机网络/Graph.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include<stdio.h>
+#include<math.h>
+#include <iostream>
+#include <algorithm>
+#include<cstring>
+#include<string>
+#include<stack>
+#include<queue>
+
+using namespace std;
+struct Edge
+{
+	int dest;//边的另一边的节点
+	int cost;//权值
+	Edge* next;
+	Edge(int num, int ways) :dest(num), next(NULL),cost(0) {};
+};
+
+struct Vertex
+{
+	string Name;	//路由器名字
+	Edge*first;//边链表的头指针
+	Vertex() :first(NULL) {};
+};
+
+class Graph
+{
+public:
+	Graph():NodeNum(0),NodeTable(NULL){};
+	~Graph();
+	int Dijiesita(int v);		//对节点求路由表
+	void Init();				//初始化，一次性建图
+	void clear();
+	//friend ostream & operator<<(ostream& os, Graph & A);
+	//friend istream & operator>>(istream& is, Graph & A);
+	void insertVertex(int x, int y);		//加点
+	void insertEdge(int v1, int v2, int cost);	//加边
+private:
+	Vertex*NodeTable;//图节点表
+	int NodeNum;	 //节点个数
+};
+
+
+#endif // !GRAPH_H
