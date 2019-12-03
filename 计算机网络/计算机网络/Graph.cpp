3e66b4622c90781e4fedde08c4c1acd0f3df58d2
SHUSHI-warlock <43929384+SHUSHI-warlock@users.noreply.github.com>
2019-12-03 10:43:12 +0800

    节点插入的修改
    节点类加了一个占有符，插入节点里考虑使用删除后的空位，以及设置了最大节点个数，以后看能不能扩容。

--- 计算机网络/计算机网络/Graph.cpp
+++ 计算机网络/计算机网络/Graph.cpp
@@ -14,6 +14,11 @@ using namespace std;
 
 Graph::~Graph()
 {
+	clear();
+}
+
+void Graph::clear()
+{
 	//释放节点和边
 	Edge*Temp, *current;
 	for (int i = 0; i < NodeNum; i++)
@@ -28,26 +33,89 @@ Graph::~Graph()
 	}
 	delete[]NodeTable;
 	NodeTable = NULL;
-}
-Graph::clear()
-{
-
+	NodeNum = 0;
 }
 
-void Graph::insertVertex(string Name)
+bool Graph::insertVertex(string Name)
 {
-		
-	NodeNum++;
+	if (NodeNum < MaxNum)	//顺序添加
+	{
+		NodeTable[NodeNum].flag = 1;
+		NodeTable[NodeNum].Name = Name;
+		NodeTable[NodeNum].first = NULL;
+		NodeNum++;
+		return true;
+	}
+	else
+	{
+		for (int i = 0; i < MaxNum; i++)
+		{
+			if (NodeTable[i].flag == 0)
+			{
+				NodeTable[i].flag = 1;
+				NodeTable[i].Name = Name;
+				NodeTable[i].first = NULL;
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 void Graph::insertEdge(int v1, int v2, int cost)
 {
 	Edge*current;
+
 	current = NodeTable[v1].first;
-	NodeTable[v1].first = new Edge(v2, cost);
+	NodeTable[v1].first = new Edge(v2, cost);	//首插法
 	NodeTable[v1].first->next = current;
 
 	current = NodeTable[v2].first;
 	NodeTable[v2].first = new Edge(v1, cost);
 	NodeTable[v2].first->next = current;
+}
+
+void Graph::deleteVertex(int v)				//删点
+{
+	//删除该点和其他点的边的信息
+	Edge* current,*temp,*last;
+	current = NodeTable[v].first;
+	while (current!=NULL)
+	{
+			//删除这条边上另一个节点中的这条边
+		int m = current->dest;
+		temp= NodeTable[m].first;
+		if (temp->dest == v)
+		{
+			NodeTable[m].first = temp->next;
+			delete temp;
+			temp = NULL;
+		}
+		else
+		{
+			last = temp;
+			temp = temp->next;
+			while (temp != NULL)
+			{
+				if (temp->dest == v)
+				{
+					last->next = temp->next;
+					delete temp;
+					break;
+				}
+			}	
+			last = temp = NULL;
+		}
+			//删除当前节点里的这条边
+		temp = current;
+		current = current->next;
+		delete temp;
+		temp = NULL;
+	}
+	//删除点
+}
+
+void Graph::deleteEdge(int v1, int v2)		//删边
+{
+
 }
--- 计算机网络/计算机网络/Graph.h
+++ 计算机网络/计算机网络/Graph.h
@@ -5,6 +5,8 @@
 #include<stdio.h>
 #include<string>
 
+const int MaxNum = 30;
+
 struct Edge
 {
 	int dest;//边的另一边的节点
@@ -15,7 +17,8 @@ struct Edge
 
 struct Vertex
 {
-	string Name;	//路由器名字
+	bool flag;		//标志是否占有
+	string Name;	//路由器IP
 	Edge*first;//边链表的头指针
 	Vertex() :first(NULL) {};
 };
@@ -32,6 +35,8 @@ public:
 	//friend istream & operator>>(istream& is, Graph & A);
 	void insertVertex(string Name);		//加点
 	void insertEdge(int v1, int v2, int cost);	//加边
+	void deleteVertex(int v);			//删点
+	void deleteEdge(int v1, int v2);	//删边
 private:
 	Vertex*NodeTable;//图节点表
 	int NodeNum;	 //节点个数
