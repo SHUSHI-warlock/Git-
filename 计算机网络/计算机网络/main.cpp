68ec34a2446fb4c2a0faf7c1f0b9864f7dfba46f
SHUSHI-warlock <43929384+SHUSHI-warlock@users.noreply.github.com>
2019-12-10 11:22:25 +0800

    主函数

---
+++ 计算机网络/计算机网络/main.cpp
@@ -0,0 +1,72 @@
+﻿// 计算机网络.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+#include "pch.h"
+#include"Graph.h"
+#include <iostream>
+
+int main()
+{
+	Graph G;
+	cout << "从文件中读取路由信息；" << endl;
+	cout << "生成路由表中..." << endl;
+	G.Init();
+	int chooes=-1;
+	
+	/*while (chooes!=0)
+	{
+		cout << "输入收到的链路改变信息（退出请按0）：" << endl;
+		cout << "(1)代表添加一个路由器;" << endl << "(2)代表添加一条链路;" << endl;
+		cout << "(3)代表删除一个路由器;" << endl << "(4)代表删除一条链路;" << endl;
+		cout<< "(5)查看路由表;" << endl;
+		cin >> chooes;
+		switch (chooes)
+		{
+		case 1:int IP[4];
+			cout << "请输入一个路由器IP地址：" << endl;
+			cin >> IP[0]>> IP[1] >> IP[2] >> IP[3];
+			//加检测
+			G.insertVertex(IP);
+			break;
+		case 2:int IP1[4],IP2[4],cost;
+			cout << "请输入一个路由器IP地址：" << endl;
+			cin >> IP1[0] >> IP1[1] >> IP1[2] >> IP1[3];
+			cout << "请输入另一个路由器IP地址：" << endl;
+			cin >> IP2[0] >> IP2[1] >> IP2[2] >> IP2[3];
+			cout << "请输入链路的长度：" << endl;
+			cin >> cost;
+			G.insertEdge(IP1, IP2, cost);
+			G.Update();
+			break;
+		case 3:int IP[4];
+			cout << "请输入要删除的路由器IP地址：" << endl;
+			cin >> IP[0] >> IP[1] >> IP[2] >> IP[3];
+			//加检测
+			G.deleteVertex(IP);
+			G.Update();
+			break;
+		case 4:int IP1[4], IP2[4];
+			cout << "请输入要删除的路由器IP地址：" << endl;
+			cin >> IP1[0] >> IP1[1] >> IP1[2] >> IP1[3];
+			cout << "请输入另一个要删除的路由器IP地址：" << endl;
+			cin >> IP2[0] >> IP2[1] >> IP2[2] >> IP2[3];
+			G.deleteEdge(IP1, IP2);
+			G.Update();
+			break;
+		case 5:int ways;
+			cout << "1）查看所有路由器的路由表;" << endl << "2）查看指定路由器的路由表:";
+			cin >> ways;
+			if (ways == 1)
+				G.Print();
+			else
+			{
+				int IP[4];
+				cout << "请输入要查看的路由器的IP地址:";
+				cin >> IP1[0] >> IP1[1] >> IP1[2] >> IP1[3];
+				G.Print(IP);
+			}
+			break;
+		default:cout << "即将退出" << endl;
+			break;
+		}
+	}*/
+}
+
